7cabb9c732c9657e19d3bf49fa4240bc72838758
Aasim <96825200+aasimdev@users.noreply.github.com>
2026-04-15 02:29:14 +0500

    Refactor special report handling in index.php, update valid pages in router.php to include 'special-report', and enhance special-report.php with new layout and video integration for improved user experience.

--- pages/special-report.php
+++ pages/special-report.php
@@ -1,6 +1,6 @@
 
 
-<section class="bg-coming-soon bg-cover bg-center bg-no-repeat bg-black relative py-32">
+<!-- <section class="bg-coming-soon bg-cover bg-center bg-no-repeat bg-black relative py-32">
     <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
         <div class="text-6xl text-white text-center font-bold">Coming Soon</div>
         <div class="text-white text-4xl text-center font-semibold my-4 leading-snug">NewHydrogen to Announce Reaching <br class="hidden md:inline-block" />
@@ -15,4 +15,18 @@
             <button type="submit" id="webinarSubmitBtn" class="bg-green-500 text-black py-2.5 px-6 rounded-xl text-base font-semibold absolute right-0 top-0 h-12">Sign up!</button>
         </form>
     </div>
-</section>
+</section> -->
+
+
+<div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative sm:py-40 py-20">
+        <div class="sm:text-5xl text-3xl text-center font-bold">NewHydrogen Special Report - Tuesday, April 21</div>
+        <div class="lg:max-w-[995px] max-w-[830px] mx-auto relative youtube-video-wrapper sm:mt-14 mt-8" data-video-id="bZ4xhMRHFtw">
+        <iframe id="youtube-player" src="https://www.youtube.com/embed/bZ4xhMRHFtw?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+        <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
+            <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
+                <circle cx="27.8926" cy="28.1125" r="27.5" fill="white" fill-opacity="1"></circle>
+                <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
+            </svg>
+        </button>
+    </div>
+    </div>
