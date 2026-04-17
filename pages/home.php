7f995d38ff845009dd2db1fb0593647e4bb6c0d1
Aasim <96825200+aasimdev@users.noreply.github.com>
2026-04-17 15:38:24 +0500

    Enhance home.php layout by updating the "Coming Soon" section with new event details and a video embed, while adding CSS utility classes for improved styling and responsiveness.

--- pages/home.php
+++ pages/home.php
@@ -38,30 +38,18 @@
     </div>
 </section> -->
 <section class="bg-coming-soon-new bg-cover bg-center bg-no-repeat bg-black relative py-12 sm:py-32">
-    <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
-        <div class="text-5xl sm:text-6xl text-white text-center font-bold">Coming Soon</div>
-        <div class="text-5xl sm:text-6xl text-white text-center font-bold">
-            NewHydrogen to make
+    <div class="px-2 sm:px-4 z-10 relative">
+        <div class="text-5xl sm:text-6xl text-white text-center font-bold sm:leading-[76px]">
+            NewHydrogen Completes critical 
             <br class="hidden md:inline-block" />
-            a Special Announcement
+            Pre-Pilot Plant Technical Validation
         </div>
-        <div class="text-white text-2xl sm:text-4xl text-center font-semibold my-4 leading-snug">
-        Tuesday, April 21, 2026 
-            <br class="hidden md:inline-block" />
-            4:00 pm ET / 1:00 pm PT
-        </div>
-        <p class="text-2xl text-center text-white font-medium">
-        Sign up to receive the viewing link and join the broadcast.
+        <p class="text-2xl text-center text-white font-medium mt-4">
+            View Special Report Video Below
         </p>
-
-        <form id="jotformForm" action="https://submit.jotform.com/submit/250137276726055/" method="POST" method="POST" autocomplete="off" class="max-w-[400px] mx-auto relative mt-4">
-            <input type="hidden" name="formID" value="250137276726055">
-                <input type="email" name="q3_webinaremail" id="input_3" placeholder="Email" class="bg-white w-full py-2.5 pl-7 pr-40 rounded-full h-12 text-black focus-within:outline-0">
-                <button type="submit" id="submitButtonWebinar" class="bg-custom-green-200 text-black py-2.5 px-7 h-12  rounded-full inline-flex items-center justify-center font-bold text-base absolute right-0 top-0 hover:text-black">sign up!</button>
-            </form>
         
-        <!-- <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="xsT6_tSNjUs">
-            <iframe id="youtube-player" src="https://www.youtube.com/embed/xsT6_tSNjUs?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+        <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="bZ4xhMRHFtw">
+            <iframe id="youtube-player" src="https://www.youtube.com/embed/bZ4xhMRHFtw?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
           
             <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
                 <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
@@ -69,7 +57,7 @@
                     <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
                 </svg>
             </button>
-        </div> -->
+        </div>
     </div>
 </section>
 <section class="bg-banner bg-cover bg-[18%_100%] md:bg-center relative md:pb-36 md:pt-28 py-16 ">
