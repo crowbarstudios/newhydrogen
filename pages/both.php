fc2eb280551c1fc1887be440edf1d1780dd75b56
Aasim <96825200+aasimdev@users.noreply.github.com>
2026-04-20 01:21:24 +0500

    Update 'both' page layout with new padding utilities and video embed, enhancing visual hierarchy and responsiveness. Add new CSS classes for consistent spacing across sections.

--- pages/both.php
+++ pages/both.php
@@ -17,22 +17,22 @@
             </div>
     </div>
 </section> -->
-<section class="bg-coming-soon-new bg-cover bg-center bg-no-repeat bg-black relative py-12 sm:py-32">
+<section class="bg-coming-soon-new bg-cover bg-center bg-no-repeat bg-black relative pt-16 pb-12 sm:pb-32">
 <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
     <div class="grid lg:grid-cols-2">
-        <div class="px-2 sm:px-4 z-10 relative">
-            <div class="text-5xl text-white text-center font-bold leading-tight">
+        <div class="px-2 sm:px-4 z-10 relative pt-[75px]">
+            <div class="text-3xl text-white text-center font-bold leading-tight">
                 NewHydrogen Completes critical
                 Pre-Pilot Plant Technical Validation
             </div>
-            <p class="text-2xl text-center text-white font-medium mt-4">
+            <p class="text-lg text-center text-white font-medium mt-4">
                 View Special Report Video Below
             </p>
 
-            <div class="max-w-[830px] w-full min-h-[466px] mx-auto relative bg-custom-gray-300 mt-5"></div>
+            <!-- <div class="max-w-[830px] w-full min-h-[466px] mx-auto relative bg-custom-gray-300 mt-5"></div> -->
 
-            <!-- <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="xsT6_tSNjUs">
-            <iframe id="youtube-player" src="https://www.youtube.com/embed/xsT6_tSNjUs?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+            <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="bZ4xhMRHFtw">
+            <iframe id="youtube-player" src="https://www.youtube.com/embed/bZ4xhMRHFtw?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
           
             <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
                 <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
@@ -40,15 +40,15 @@
                     <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
                 </svg>
             </button>
-        </div> -->
+        </div>
         </div>
         <div class="px-2 sm:px-4 z-10 relative order-first lg:order-1">
-            <div class="text-5xl text-white text-center font-bold leading-tight">
+            <div class="text-3xl text-white text-center font-bold leading-tight">
                 NewHydrogen and NuCube Energy Announce
                
                 Strategic Partnership to Explore Nuclear-Driven Clean Hydrogen Production
             </div>
-            <p class="text-2xl text-center text-white font-medium mt-4">
+            <p class="text-lg text-center text-white font-medium mt-4">
                 View Special Report Video Below
             </p>
 
