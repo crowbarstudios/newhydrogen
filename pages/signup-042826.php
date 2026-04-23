a37e20c62438441494b024c319ff436cd94674b3
aasimdev <aasimdev69@gmail.com>
2026-04-23 20:26:26 +0500

    Build Signup-042826 page

---
+++ pages/signup-042826.php
@@ -0,0 +1,30 @@
+<section class="relative py-12 sm:py-32">
+    <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
+        <div class="text-4xl sm:text-6xl text-black text-center font-bold">Coming Soon</div>
+        <div class="text-[30px] sm:text-[50px] text-black text-center font-bold my-4 leading-9 sm:leading-[64px]">
+            NewHydrogen to make
+            <br class="hidden md:inline-block" />
+            a Special Announcement
+        </div>
+        <p class="text-2xl text-center text-black font-medium pt-3 pb-5">
+            Register now to watch this Special Report when it premieres on <br class="hidden md:inline-block"> Tuesday, April 28, 2026, at 4 PM EDT
+        </p>
+
+        <form id="jotformForm" action="https://submit.jotform.com/submit/250137276726055/" method="POST" method="POST" autocomplete="off" class="max-w-[400px] mx-auto relative mt-4">
+            <input type="hidden" name="formID" value="250137276726055">
+                <input type="email" name="q3_webinaremail" id="input_3" placeholder="Email" class="bg-white w-full py-2.5 pl-7 pr-40 rounded-full h-12 text-black outline outline-[1.6px] outline-black">
+                <button type="submit" id="submitButtonWebinar" class="bg-custom-green-200 text-black py-2.5 px-7 h-12  rounded-full inline-flex items-center justify-center font-bold text-base absolute right-0 top-0 hover:text-black">sign up!</button>
+            </form>
+        
+        <!-- <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="xsT6_tSNjUs">
+            <iframe id="youtube-player" src="https://www.youtube.com/embed/xsT6_tSNjUs?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+          
+            <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
+                <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
+                    <circle cx="27.8926" cy="28.1125" r="27.5" fill="white" fill-opacity="1"></circle>
+                    <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
+                </svg>
+            </button>
+        </div> -->
+    </div>
+</section>
