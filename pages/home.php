ee8fef3b850141deeecaa69a8c35f3067c8debb9
Aasim <96825200+aasimdev@users.noreply.github.com>
2026-04-25 22:45:37 +0500

    Update home.php to adjust text sizes for better responsiveness and replace the video reference with a new YouTube link for the special report. Commented-out sections have been removed for cleaner code.

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,7 @@
 <?php $videos = include "./data/carousel-videos.php"; ?>
 <section class="bg-coming-soon-new-updated bg-cover bg-center bg-no-repeat bg-black relative py-12">
     <div class="px-2 sm:px-4 z-10 relative">
-        <div class="text-5xl text-black text-center font-bold !leading-[64px]">
+        <div class="sm:text-5xl text-3xl text-black text-center font-bold sm:!leading-[64px]">
         NewHydrogen and NuCube Energy Announce
 
             <br class="hidden md:inline-block" />
@@ -11,10 +11,9 @@
             View Special Report Video Below
         </p>
 
-        <div class="max-w-[830px] w-full min-h-[466px] mx-auto relative bg-custom-gray-300 mt-5"></div>
         
-        <!-- <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="xsT6_tSNjUs">
-            <iframe id="youtube-player" src="https://www.youtube.com/embed/xsT6_tSNjUs?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+        <div class="max-w-[830px] mx-auto relative youtube-video-wrapper mt-5" data-video-id="okieIiBmC8s">
+            <iframe id="youtube-player" src="https://www.youtube.com/embed/okieIiBmC8s?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
           
             <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
                 <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
@@ -22,7 +21,7 @@
                     <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
                 </svg>
             </button>
-        </div> -->
+        </div>
     </div>
 </section>
 <!-- <section class="bg-coming-soon-new bg-cover bg-center bg-no-repeat bg-black relative py-12 sm:py-32">
