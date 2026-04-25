524efdcdbf73a8b8953cbcb217e23bb6db7abeeb
Aasim <96825200+aasimdev@users.noreply.github.com>
2026-04-25 22:09:43 +0500

    Add new press release for NewHydrogen's collaboration with NuCube Energy on nuclear-powered clean hydrogen production, including detailed descriptions and updates in news-data.php. Update video reference in special-report-nucube.php for enhanced content presentation.

--- pages/special-report-nucube.php
+++ pages/special-report-nucube.php
@@ -24,14 +24,14 @@
             NewHydrogen and NuCube Energy Announce <br class="hidden md:inline-block" />
             Collaboration to Explore Nuclear-Driven Clean Hydrogen Production
         </h1>
-        <div class="max-w-[830px] w-full min-h-[466px] mx-auto relative bg-custom-gray-300 mt-5"></div>
-        <!-- <div class="lg:max-w-[995px] max-w-[830px] mx-auto relative youtube-video-wrapper sm:mt-14 mt-8" data-video-id="bZ4xhMRHFtw">
-        <iframe id="youtube-player" src="https://www.youtube.com/embed/bZ4xhMRHFtw?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+        <!-- <div class="max-w-[830px] w-full min-h-[466px] mx-auto relative bg-custom-gray-300 mt-5"></div> -->
+        <div class="lg:max-w-[995px] max-w-[830px] mx-auto relative youtube-video-wrapper sm:mt-14 mt-8" data-video-id="okieIiBmC8s">
+        <iframe id="youtube-player" src="https://www.youtube.com/embed/okieIiBmC8s?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
         <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
             <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
                 <circle cx="27.8926" cy="28.1125" r="27.5" fill="white" fill-opacity="1"></circle>
                 <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
             </svg>
-        </button> -->
+        </button>
     </div>
 </div>
