b59bc94b00bc0b0479ea61942d8ab84d79055569
cancervax <129687886+cancervax@users.noreply.github.com>
2026-08-29 16:16:51 +0000

    Center newsletter captcha across website

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,22 @@
+<style>
+    body .newsletter-captcha-field {
+        text-align: center;
+    }
+
+    body .newsletter-captcha-field label {
+        display: block;
+        width: 100%;
+        text-align: center;
+    }
+
+    body .newsletter-captcha-field .form-input-wide,
+    body .newsletter-captcha-field section,
+    body .newsletter-captcha-field .h-captcha {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
 <footer class="bg-custom-gray-dark-100 <?php echo ($page === 'fasttrack') ? 'pt-10' : 'pt-20'; ?> pb-14">
     <div class="mx-auto max-w-screen-xl px-2 sm:px-4">
         <?php if ($page !== 'fasttrack') { ?>
@@ -7,7 +26,7 @@
             <input type="hidden" name="formID" value="242986385047065">
                 <input type="email" name="q3_email" id="input_3" placeholder="Email" class="bg-custom-gray-dark-200 text-white w-full py-2.5 pl-7 pr-40 rounded-full h-12 focus-within:outline-0">
 
-                <div class="<?php echo ($page === 'bigh-aug-27-a') ? 'bigh-newsletter-captcha-field ' : ''; ?>form-line captcha-field jf-required mt-3" data-type="control_captcha" id="id_4"><label class="form-label form-label-top form-label-auto text-white mb-1" id="label_4" for="input_5" aria-hidden="false"> Please verify that you are human<span class="form-required">*</span> </label>
+                <div class="newsletter-captcha-field form-line captcha-field jf-required mt-3" data-type="control_captcha" id="id_4"><label class="form-label form-label-top form-label-auto text-white mb-1" id="label_4" for="input_5" aria-hidden="false"> Please verify that you are human<span class="form-required">*</span> </label>
                         <div id="cid_4" class="form-input-wide jf-required" data-layout="full">
                             <section data-wrapper-react="true">
                                 <div id="hcaptcha_input_5" class="h-captcha" data-siteKey="772f4a50-7161-425e-8cd5-4d7e361ab765" data-callback="hcaptchaCallbackinput_5" data-expired-callback="hcaptchaExpiredCallbackinput_5"></div><input type="hidden" id="input_5" class="hidden validate[required]" name="hcaptcha_visible" required="" />
